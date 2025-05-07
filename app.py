5944f5e2f3168cbd6d2e3c3d54a8efbff75004dd
Mayank-Creater <programmayank@gmail.com>
2025-05-07 20:14:52 +0530

    Fixed render issues

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ async def user_remove(email: str, password: str):
 
 
 if __name__ == '__main__':
-    uvicorn.run("app:app", host="127.0.0.1", port=8000)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
